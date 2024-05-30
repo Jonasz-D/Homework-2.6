d9880379f522b41b4e3b938118ba427652caddb4
Jonasz-D <jdanisz@interia.pl>
2024-05-30 19:55:30 +0200

    v2

--- run_all_querries.py
+++ run_all_querries.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-NUM_OF_QUERRIES = 10
+NUM_OF_QUERIES = 10
 
 def run_querry(inner_path):
     with open(inner_path, 'r') as f:
@@ -12,9 +12,9 @@ def run_querry(inner_path):
         return cur.fetchall()
 
 if __name__ == '__main__':
-    for querry in range(1, NUM_OF_QUERRIES + 1):
-        inner_path = f'querries/querry_{querry}.sql'
-        msg = f'\nQuerry number: {querry}\n'
+    for query in range(1, NUM_OF_QUERIES + 1):
+        inner_path = f'queries/query_{query}.sql'
+        msg = f'\nQuery number: {query}\n'
         print(msg)
         for el in run_querry(inner_path):
             print(el)
